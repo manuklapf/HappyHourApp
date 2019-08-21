b3cf440c45c79e9bfdd977abdb2ed6f0d017431e
LissyR <53035676+LissyR@users.noreply.github.com>
2019-08-21 13:01:34 +0200

    currentLocation

--- app/src/main/java/com/example/happyhourapp/MainActivityMap.java
+++ app/src/main/java/com/example/happyhourapp/MainActivityMap.java
@@ -26,22 +26,13 @@ public class MainActivityMap extends FragmentActivity implements OnMapReadyCallb
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker current location
+        LatLng current = new LatLng(0, 0);
+        mMap.addMarker(new MarkerOptions().position(current).title("Dein Standort"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
     }
 }
